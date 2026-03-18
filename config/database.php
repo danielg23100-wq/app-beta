e2a84b3c6a907fe9745d45fd88024ed40804e170
Daniel Guaranguay <daniel.g23100@gmail.com>
2026-03-18 08:38:53 -0500

    Connection Servers.

--- config/database.php
+++ config/database.php
@@ -7,13 +7,13 @@
     $LOCAL_PORT     = '5432';
 
     //Supabase Database configuration
-    $SUPA_HOST      = '';
-    $SUPA_DBNAME    = '';
-    $SUPA_USERNAME  = '';
-    $SUPA_PASSWORD  = '';
-    $SUPA_PORT      = '';
+    $SUPA_HOST      = 'aws-0-us-west-2.pooler.supabase.com';
+    $SUPA_DBNAME    = 'postgres';
+    $SUPA_USERNAME  = 'postgres.jhklctlcgozmkrerafxd';
+    $SUPA_PASSWORD  = 'unicesmagQQ';
+    $SUPA_PORT      = '6543';
 
-    $data_connection = "
+    $local_data_connection = "
        host      = $LOCAL_HOST
        dbname    =$LOCAL_DBNAME
        user      =$LOCAL_USERNAME
@@ -21,13 +21,30 @@
        port      =$LOCAL_PORT
     ";
 
-    $conn =pg_connect ($data_connection);
+    $supa_data_connection = "
+       host      = $SUPA_HOST
+       dbname    =$SUPA_DBNAME
+       user      =$SUPA_USERNAME
+       password  =$SUPA_PASSWORD
+       port      =$SUPA_PORT
+    ";
 
-    if(!$conn){
-        echo "Error: Unable to connect to basedate. ";
+    $local_conn =pg_connect ($local_data_connection);
+
+    if(!$local_conn){
+        echo "Error: Unable to connect to local basedate. ";
         exit();
     }else{
-        echo "Success connection !!!";
+        echo "Local success connection !!!";
+    }
+
+    $supa_conn =pg_connect ($supa_data_connection);
+
+    if(!$supa_conn){
+        echo "Error: Unable to connect to supa base basedate. ";
+        exit();
+    }else{
+        echo "<br> Supa success connection !!!";
     }
 
 ?>

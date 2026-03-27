2b7dda06bd53854e313d1f7c322961e4ba6bac73
Daniel Guaranguay <daniel.g23100@gmail.com>
2026-03-27 11:58:00 -0500

    Segundo Punto phone

--- src/signup.php
+++ src/signup.php
@@ -15,12 +15,24 @@ $enc_pass = md5($p_sswd);
 
 // Funcionalidad 1: Validar unicidad del correo electrónico
 $check_email = "SELECT email FROM users_model WHERE email = '$e_mail'";
-$res_email = pg_query($local_conn, $check_email); // Asumiendo que $db_connection viene de database.php
+$res_email = pg_query($local_conn, $check_email); 
 
 if (pg_num_rows($res_email) > 0) {
     echo "Error: El correo electrónico ya se encuentra registrado.";
     exit; // Detiene la ejecución
 }
+
+
+// Funcionalidad 2: Validar unicidad del número móvil
+$check_phone = "SELECT mobile_phone FROM users_model WHERE mobile_phone = '$m_phone'";
+$res_phone = pg_query($local_conn, $check_phone);
+
+if (pg_num_rows($res_phone) > 0) {
+    echo "Error: El número de teléfono móvil ya está vinculado a otra cuenta.";
+    exit;
+}
+
+
 
 $sql = "INSERT INTO users_model (firstname, lastname, email, mobile_phone, password)
         VALUES ('$f_name', '$l_name', '$e_mail', '$m_phone', '$enc_pass')";
